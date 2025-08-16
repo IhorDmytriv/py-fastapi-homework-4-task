0e7d611e3f7fb4b766eea0b54115cbf1c5f7e8d6
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-08-16 15:13:04 +0200

    Create validation for info field

--- src/validation/profile.py
+++ src/validation/profile.py
@@ -43,3 +43,9 @@ def validate_birth_date(birth_date: date) -> None:
     age = (date.today() - birth_date).days // 365
     if age < 18:
         raise ValueError('You must be at least 18 years old to register.')
+
+
+def validate_info(info: str) -> None:
+    info = info.strip()
+    if not info:
+        raise ValueError("Info field cannot be empty or contain only spaces.")
